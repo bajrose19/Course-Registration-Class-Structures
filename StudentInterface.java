9874fda0b9371439b1c0173d3d5df72df8be28ad
chloe-sundstrom12 <cms10046@nyu.edu>
2026-02-19 14:57:35 -0500

    StudentInterface.java

--- StudentInterface.java
+++ StudentInterface.java
@@ -7,9 +7,9 @@ public interface StudentInterface {
 	
 	ArrayList<Course> viewRegisteredCourses();
 	ArrayList<Course> viewAvaliableCourses(ArrayList<Course> courses);
-	boolean courseRegistration(Course courses);
-	boolean courseWithdrawl(Course courses);
+	boolean courseRegistration(String courseId, ArrayList<Course> courses);
+	boolean courseWithdrawl(String courseId, ArrayList<Course> courses);
 	ArrayList<Course> viewAllCourses(ArrayList<Course> courses);
-	void displayMenu();
-	boolean courseWithdrawal(Course courses);
+
 }
+
