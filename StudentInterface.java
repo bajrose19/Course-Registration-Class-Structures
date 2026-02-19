82ecf413a36a0f6e2911e50def286da655788656
chloe-sundstrom12 <cms10046@nyu.edu>
2026-02-19 17:31:45 -0500

    Update StudentInterface.java

--- StudentInterface.java
+++ StudentInterface.java
@@ -2,14 +2,19 @@ package hw1;
 
 import java.util.ArrayList;
 
+/**
+ * StudentInterface defines the actions a Student can perform.
+ * Implements: ADT concept (Req 06)
+ */
 public interface StudentInterface {
-	// methods the student class will be implementing 
-	
-	ArrayList<Course> viewRegisteredCourses();
-	ArrayList<Course> viewAvaliableCourses(ArrayList<Course> courses);
-	boolean courseRegistration(String courseId, ArrayList<Course> courses);
-	boolean courseWithdrawl(String courseId, ArrayList<Course> courses);
-	ArrayList<Course> viewAllCourses(ArrayList<Course> courses);
 
+    // Course Management (Req 04)
+    ArrayList<Course> viewAllCourses();
+    ArrayList<Course> viewAvailableCourses();      // not full courses
+    boolean courseRegistration(Course c);
+    boolean courseWithdrawal(Course c);
+    ArrayList<Course> viewRegisteredCourses();
+
+    // Helper
+    boolean isRegisteredIn(Course c);
 }
-
