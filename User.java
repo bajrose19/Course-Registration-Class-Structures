67c135616b583b3d32a0f93c900bcb803627cb03
chloe-sundstrom12 <cms10046@nyu.edu>
2026-02-19 17:32:44 -0500

    Update User.java

--- User.java
+++ User.java
@@ -4,16 +4,17 @@ import java.io.Serializable;
 
 /**
  * Abstract User Class (Parent of Admin and Student)
- * Demonstrates Inheritance, Encapsulation, and Abstraction.
+ * Demonstrates: Inheritance, Encapsulation, Abstraction
  */
 public abstract class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     protected String username;
     protected String password;
     protected String firstName;
     protected String lastName;
 
-    // Constructor
     public User(String username, String password, String firstName, String lastName) {
         this.username = username;
         this.password = password;
@@ -21,42 +22,23 @@ public abstract class User implements Serializable {
         this.lastName = lastName;
     }
 
-    // Abstract method (will be overridden in Admin and Student)
+    // Abstract method - overridden by Admin and Student (Method Overriding)
     public abstract void displayMenu();
 
     // Getters (Encapsulation)
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername()  { return username; }
+    public String getPassword()  { return password; }
+    public String getFirstName() { return firstName; }
+    public String getLastName()  { return lastName; }
+    public String getFullName()  { return firstName + " " + lastName; }
 
-    public String getPassword() {
-        return password;
-    }
+    // Setters
+    public void setUsername(String username)   { this.username = username; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName)   { this.lastName = lastName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    // Setters (Encapsulation)
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    // Method Overloading example (different logic for password setting)
+    // Method Overloading: two setPassword versions
     public void setPassword(String newPassword) {
-        // Password must be at least 8 characters
         if (newPassword != null && newPassword.length() >= 8) {
             this.password = newPassword;
         } else {
@@ -64,20 +46,21 @@ public abstract class User implements Serializable {
         }
     }
 
-    // Returns full name
-    public String getFullName() {
-        return firstName + " " + lastName;
+    public void setPassword(String oldPassword, String newPassword) {
+        if (!this.password.equals(oldPassword)) {
+            System.out.println("Old password is incorrect.");
+            return;
+        }
+        setPassword(newPassword);
     }
 
-    // Login validation method
+    // Login validation
     public boolean login(String username, String password) {
-        return this.username.equals(username) &&
-               this.password.equals(password);
+        return this.username.equals(username) && this.password.equals(password);
     }
 
     @Override
     public String toString() {
-        return "Username: " + username +
-               "\nName: " + getFullName();
+        return "Username: " + username + "\nName: " + getFullName();
     }
 }
